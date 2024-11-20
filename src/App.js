40f93e357b269ab1a33cbfebcdb7663bee4d105c
Sanzhar Kiyassov <sanzhar@April.local>
2024-11-20 11:34:56 +0500

    fc

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import axios from "axios";
 import './App.css';
 
 function App() {
+
+const [state, setState] = useState([]);
+
+
+
+const getEmployees = () => {
+  
+  
+  axios.get("https://dummy.restapiexample.com/api/v1/employees")
+    .then((response) => {
+      console.log(response);
+    });
+};
+
+  
+  
+
+  useEffect(() => {
+    getEmployees();
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
