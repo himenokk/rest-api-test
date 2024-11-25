fead123e0f14a4b002f9dd6ab5ff562309e0d6d0
Sanzhar Kiyassov <sanzhar@April.local>
2024-11-25 12:39:26 +0500

    after redux

--- src/App.js
+++ src/App.js
@@ -1,46 +1,20 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import './App.css';
+import { fetchUserAction } from "./actions/fetchUserAction";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
 
-const [state, setState] = useState([]);
-const [name, setName] = useState("");
-const [username, setUsername] = useState("");
-const [email, setEmail] = useState("");
+const { userList } = useSelector((state) => state.userReducer);
+const dispatch = useDispatch();
+
 
 const getEmployees = () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/users")
-    .then((response) => {
-      setState(response.data);
-    });
+  dispatch(fetchUserAction());
 };
 
-//"name": "Ervin Howell",
-// "username": "Antonette",
-// "email": "Shanna@melissa.tv"
-  
-const handleEmployeeDelete = (id) => {
-  axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`).then(() => {
-    setState((prevState) => prevState.filter((el) => el.id !== id));
-  });
-};
 
-const HandleCreate = (e) => {
-  e.preventDefault();
-  let body = {
-
-    name,
-    username,
-    email,
-  }
-  axios.post(`https://jsonplaceholder.typicode.com/users/1/posts`, body)
-  .then(response => {
-    console.log(response)
-  });
-  console.log("submit");
-}
 
 useEffect(() => {
   getEmployees();
@@ -51,7 +25,7 @@ useEffect(() => {
 
   return (
     <div className="App">
-      <form onSubmit={HandleCreate}>
+      {/* <form onSubmit={HandleCreate}>
         <h1>Create new user</h1>
         <input 
         onChange={(event) => setName(event.target.value)} 
@@ -72,14 +46,13 @@ useEffect(() => {
         />
         <br/>
         <button type="submit">CREATE</button>
-      </form>
+      </form> */}
       
-      {state.map((el) => (
-
+      {userList.map((el) => (
         <div key={el.id} className="employees">
         <div>{el.id}</div>
         <div>{el.name}</div>
-        <button onClick={() => handleEmployeeDelete(el.id)}>Delete</button>
+        {/* <button onClick={() => handleEmployeeDelete(el.id)}>Delete</button> */}
         </div>
       ))}
 
